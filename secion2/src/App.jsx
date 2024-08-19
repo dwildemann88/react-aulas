6779ca9a46c2a499e524017fb9650adc0171a149
dwildemann88 <145924257+dwildemann88@users.noreply.github.com>
2024-08-19 14:56:23 -0300

    iamgem assests

--- secion2/src/App.jsx
+++ secion2/src/App.jsx
@@ -2,13 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import City from './assets/img2.jpg'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <div className="App">
+        {/* imagem em assests */}
+        <div className="img">
+          <img src={City} alt="cidade" />
+        </div>
+      </div>
     </>
   )
 }
